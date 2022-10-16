b8d66fedf4962e19341b3ca9e880b13277e94dfe
parksangau <devsangau@gmail.com>
2022-10-16 20:39:03 +0900

    Initial Commit

---
+++ ImageProcessingProgram/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ImageProcessingProgram
+//
+//  Created by 박상은 on 2022/10/16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
